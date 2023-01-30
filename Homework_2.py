a59998dd4222b32e97f16d990524b8394bd0e6f1
SDSW-DrHicks <119824568+SDSW-DrHicks@users.noreply.github.com>
2023-01-30 15:23:26 -0500

    second commit

--- Homework_2.py
+++ Homework_2.py
@@ -1,23 +1,44 @@
 #-----------------------------------------------------
-# Homework 1: Graphs, plots, and functions
+# Homework 1: Plots and functions
 #-----------------------------------------------------
-# 25 points total (1 point = 4% of grade)
+# 20 points total (1 point = 5% of grade)
 # This assignment will count as a "Journal" grade.
 # You may use class notes or visit office hours.
 # You cannot work on this assignment as a group.
 
-# For each questions, use the comment symbol (#) to explain the code.
+# For each question, use the comment symbol (#) to explain the code.
 # Only one or 2 sentences are necessary. This is important for partial credit.
 # No comments, no credit.
 #-----------------------------------------------------
 
 # Problem 1:
 # Import matplotlib and numpy
+# (2 points)
 # -------
-# Use this space to write code for probelm 1.
+# Use this space to write code for problem 1.
 #--------
 
-# Problem 2:
+# Problem 2a:
 # For the function: y = x**2+3*x+1
 # Write a function code (using "def") to take a user-input value
-# to
+# of "x" and print the solution 'y.'
+# (5 points)
+#--------
+# Use this space to write code for problem 2a.
+#--------
+
+# Problem 2b:
+# Use the pyplot feature of matplotlib to generate
+# a plot of the above function (from 2b) for values of 'x'
+# from [-15,15].
+# (3 points)
+#--------
+# Use this space to write code for problem 2b.
+#--------
+
+# Problem 3:
+# The following equation is used to predict the change
+# in the concentration of a source
+# (10 points)
+#-------
+# Hint
