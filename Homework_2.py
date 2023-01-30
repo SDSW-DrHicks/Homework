0b7c9965a4d9b28296664c5522b3e2f46abbe60d
SDSW-DrHicks <119824568+SDSW-DrHicks@users.noreply.github.com>
2023-01-30 15:14:40 -0500

    First Commit

---
+++ Homework_2.py
@@ -0,0 +1,23 @@
+#-----------------------------------------------------
+# Homework 1: Graphs, plots, and functions
+#-----------------------------------------------------
+# 25 points total (1 point = 4% of grade)
+# This assignment will count as a "Journal" grade.
+# You may use class notes or visit office hours.
+# You cannot work on this assignment as a group.
+
+# For each questions, use the comment symbol (#) to explain the code.
+# Only one or 2 sentences are necessary. This is important for partial credit.
+# No comments, no credit.
+#-----------------------------------------------------
+
+# Problem 1:
+# Import matplotlib and numpy
+# -------
+# Use this space to write code for probelm 1.
+#--------
+
+# Problem 2:
+# For the function: y = x**2+3*x+1
+# Write a function code (using "def") to take a user-input value
+# to
